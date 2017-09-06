eb448449044b8828753d9f14a29d47392ec4a0b9
TJ <tjspann@users.noreply.github.com>
2017-09-06 10:48:54 -0500

    Update StackSetup.sh
    StackSetup.sh finalized.

--- StackSetup.sh
+++ StackSetup.sh
@@ -3,15 +3,15 @@
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 
-#########################     Sanity Fixes     #########################
-# Ensure script has sudo before doing anything.
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root."
    exit 1
 fi
-echo "DIR is: "$DIR
 if [ $DIR != "/tmp" ]; then
    cd /tmp
 fi
+wget https://raw.githubusercontent.com/tjspann/StackSetup/experimental/master.sh
+chmod +x /tmp/master.sh
+/bin/bash /tmp/master.sh
 
 exit 0
